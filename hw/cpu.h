6c9c4a7099e2d6dbec7d29e78a1b24cdfebbf9ac
Viktor Hansen <viktor.f.hansen@gmail.com>
2016-07-16 05:43:43 +0200

    Added script to compile and run tetris. Indentation using only spaces. Read first bank of cartridge and print game title encoded in cartridge ROM.

--- hw/cpu.h
+++ hw/cpu.h
@@ -6,63 +6,63 @@
 /* TODO: Document */
 
 typedef struct {
-	// AF register
-  	union {
-  		struct {
-  			ubyte_t F;
-  			ubyte_t A;
-  		};
-  		uword_t AF;
-  	};
-	// BC register
-  	union {
-  		struct {
-  			ubyte_t C;
-  			ubyte_t B;
-  		};
-  		uword_t BC;
-  	};
-	// DE register 
-  	union {
-  		struct {
-  			ubyte_t E;
-  			ubyte_t D;
-  		};
-  		uword_t DE;
-  	};
-	// HL register
-  	union {
-  		struct {
-  			ubyte_t L;
-  			ubyte_t H;
-  		};
-  		uword_t HL;
-  	};
-	// Flags register
-	struct {
-		// Carry flag:
-		// This bit is set if a carry occurred from the last
+    // AF register
+    union {
+        struct {
+            ubyte_t F;
+            ubyte_t A;
+        };
+        uword_t AF;
+    };
+    // BC register
+    union {
+        struct {
+            ubyte_t C;
+            ubyte_t B;
+        };
+        uword_t BC;
+    };
+    // DE register 
+    union {
+        struct {
+            ubyte_t E;
+            ubyte_t D;
+        };
+        uword_t DE;
+    };
+    // HL register
+    union {
+        struct {
+            ubyte_t L;
+            ubyte_t H;
+        };
+        uword_t HL;
+    };
+    // Flags register
+    struct {
+        // Carry flag:
+        // This bit is set if a carry occurred from the last
         // math operation or if register A is the smaller value
         // when executing the CP instruction.
-		ubyte_t C : 1;
-		// Half carry flag:
-		// This bit is set if a carry occurred from the lower
-		// nibble in the last math operation.
-		ubyte_t H : 1;
-		// Subtract flag:
-		// This bit is set if a subtraction was performed in the
-			// last math instruction.
-		ubyte_t N : 1;
-		// Zero flag:
-		// This bit is set when the result of a math operation
-		// is zero or two values match when using the CP
-		// instruction.
-		ubyte_t Z : 1;
-	} flags;
-	// Stack pointer - should be initialized to 0x100 upon startup.
-  	uword_t SP;
+        ubyte_t C : 1;
+        // Half carry flag:
+        // This bit is set if a carry occurred from the lower
+        // nibble in the last math operation.
+        ubyte_t H : 1;
+        // Subtract flag:
+        // This bit is set if a subtraction was performed in the
+        // last math instruction.
+        ubyte_t N : 1;
+        // Zero flag:
+        // This bit is set when the result of a math operation
+        // is zero or two values match when using the CP
+        // instruction.
+        ubyte_t Z : 1;
+    } flags;
+    // Stack pointer - should be initialized to 0x100 upon startup.
+    uword_t SP;
     // Program counter
-  	uword_t PC;
+    uword_t PC;
 } registers_t;
 
 #endif
--- main.c
+++ main.c
@@ -2,40 +2,57 @@
 #include <stdlib.h>
 
 #include "hw/cpu.h"
-//#include "hw/cartridge.h"
+#include "hw/cartridge.h"
 #include "hw/memory.h"
 
+#include "core/types.h"
+
 void die(char* message) {
-	printf("%s! Aborting...\n", message);
-	exit(EXIT_FAILURE);
+    printf("%s! Aborting...\n", message);
+    exit(EXIT_FAILURE);
 }
 
 int main(int argc, char* argv[]) {
-	registers_t regs;
+    registers_t regs;
 
     // Check if a filename was specified.
     if (argc < 2)
-    	die("No arguments specified");
+        die("No arguments specified");
 
     FILE *rom_fp;
 
     // Read the file
-	char* filename = argv[1];
-	rom_fp = fopen(filename, "rb");
+    char* filename = argv[1];
+    rom_fp = fopen(filename, "rb");
 
     if (rom_fp == NULL)
-    	die("File does not exist");
+        die("File does not exist");
 
     char ch;
+
+    // Read the first bank of the cartridge into memory
+    ubyte_t mem[MEM_SIZE];
+    memory_t memory = mem;
+
+    for (int i = 0; i < CARTRIDGE_BANK_SIZE; ++i) {
+        memory[i] = fgetc(rom_fp);
+    }
+
+    // Print game title, just to test things
+    printf("Game title is: ");
+    for (int i = CARTRIDGE_GAME_TITLE_LOWER; i <= CARTRIDGE_GAME_TITLE_UPPER; ++i)
+        printf("%c", memory[i]);
+
+    printf("\n");
 
     /*
     while( !feof(rom_fp) ) {
       rom_data++ = fgetc(rom_fp);
     }
     rom_data = EOF;
-	*/
+    */
 
     fclose(rom_fp);
-	
-	return 0;
+    
+    return 0;
 } 
